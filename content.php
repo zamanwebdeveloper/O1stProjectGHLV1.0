b6b44ed309ed54ce2b2bed5c1c5f8ece758f08fd
Syed Zaman Mostafiz <ZamanWebDeveloper@gmail.com>
2019-03-11 16:56:59 +0600

    Office 1st Project GHL Updated in new way.

---
+++ content.php
@@ -0,0 +1,303 @@
+
+			        <!-- services start -->
+					<section class="service-home service-home2 section-padding">
+			            <div class="container">
+			              <div class="row">
+			                <div class="col-xs-12 text-center">
+			                  <h2 class="section-title">Our 24 Hour’s Services</h2>
+			                  <span class="section-sub">Nullam ac urna ey felis dapibus<br> condimeytum sit amet</span>
+			                </div>
+			              </div> <!-- /.row -->
+
+			              <div class="row content-row">
+			              	<div class="col-sm-4">
+			              		<div class="service">
+				              		<div class="service-thumb-home thumb-grid">
+				              			<a href="#"><img src="img/service/thumbgrid1.png" alt=""></a>
+				              		</div>
+				              		
+				              		<h3>Transportation &amp; Logistics</h3>
+				              		<p>Holisticly synergize maintainable intellectual<br> capital via viral interfaces. Energistically<br> embrace funyctionalized </p>
+				              		<a class="readmore" href="#">Read More &nbsp;<i class="fa fa-angle-right"></i> </a>
+			              		</div>
+			              	</div><!-- /.col-sm-4 -->
+
+			              	<div class="col-sm-4">
+			              		<div class="service">
+				              		<div class="service-thumb-home thumb-grid">
+				              			<a href="#"><img src="img/service/thumbgrid2.png" alt=""></a>
+				              		</div>
+				              		<h3>Moving &amp; Storage</h3>
+				              		<p>Holisticly synergize maintainable intellectual<br> capital via viral interfaces. Energistically<br> embrace functionalized </p>
+				              		<a class="readmore" href="#">Read More &nbsp;<i class="fa fa-angle-right"></i> </a>
+			              		</div>
+
+			              	</div><!-- /.col-sm-4 -->
+
+			              	<div class="col-sm-4">
+			              		<div class="service">
+				              		<div class="service-thumb-home thumb-grid">
+				              			<a href="#"><img src="img/service/thumbgrid3.png" alt=""></a>
+				              		</div>
+				              		<h3>Shipping Operations</h3>
+				              		<p>Holisticly synergize maintainable intellectual<br> capital via viral interfaces. Energistically<br> embrace functionalized </p>
+				              		<a class="readmore" href="#">Read More &nbsp;<i class="fa fa-angle-right"></i> </a>
+			              	   </div>
+			              	</div><!-- /.col-sm-4 -->
+			              </div> <!-- /.row -->
+			            </div><!-- /.container -->
+			        </section>
+			        <!-- services end -->
+
+			        <!-- featuer-section start -->
+			        <section class="feature-section feature-section2">
+				        <div class="container">
+				        	<div class="row">
+				        		<div class="col-sm-7 col-xs-12">
+				        			<h2>We do care about your cargo!</h2>
+
+				        			<p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p> 
+
+									<p>Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc.</p>
+
+									<a href="#" class="btn btn-primary">View Details</a>
+				        		</div>
+				        
+				        	</div>
+				        </div>
+			        </section>
+			        <!-- featuer-section end -->
+
+			        <!-- why-us-setion start -->
+			        <section class="why-us-setion section-padding">
+			        	<div class="container">
+			              <div class="row text-center">
+			                <div class="col-xs-12">
+			                  <h2 class="section-title">Why Chose Us</h2>
+			                  <span class="section-sub">Monotonectally innovate cross-media<br> resources without seamless</span>
+			                </div>
+			              </div> <!-- /.row -->
+
+			              <div class="row content-row">
+			              	<div class="col-md-12">
+								<div class="css-tab css-tab2" role="tabpanel">
+
+								  <!-- Nav tabs -->
+								  <ul class="nav nav-tabs" role="tablist">
+								    <li role="presentation" class="active"><a href="#secure" aria-controls="secure" role="tab" data-toggle="tab"><i class="flaticon-logistics15"></i></a></li>
+								    <li role="presentation"><a href="#trackable" aria-controls="trackable" role="tab" data-toggle="tab"><i class="flaticon-logistics18"></i></a></li>
+								    <li role="presentation"><a href="#fast" aria-controls="fast" role="tab" data-toggle="tab"><i class="flaticon-logistics16"></i></a></li>
+								    <li role="presentation"><a href="#reliable" aria-controls="reliable" role="tab" data-toggle="tab"><i class="flaticon-broken43"></i></a></li>
+								  </ul>
+
+								  <!-- Tab panes -->
+								  <div class="tab-content">
+								    <div role="tabpanel" class="tab-pane active fade in" id="secure">
+									    <div class="css-tab-content">
+										    <div class="row">
+										    	<div class="col-md-6 content-text">
+											    	<h3>Continually disintermediate</h3>
+
+											    	<p>Credibly build virtual materials for fully researched paradigms. Authoritatively plagiarize long-term high-impact infomediaries with goal-oriented core competencies. Conveniently visualize process-centric applications through leading-edge testing procedures. Quickly build backend customer service for real-time metrics.</p> 
+
+													<p>Efficiently transform an expanded array of methodologies rather than customized infomediaries.</p>
+										    	</div><!-- /.col-md-6 -->
+										    	<div class="col-md-6">
+										    		<img class="img-responsive" src="img/secure.png" alt="">
+										    	</div><!-- /.col-md-6 -->
+										    </div><!-- /.row -->
+									    </div><!-- /.css-tab-content -->
+								    </div>
+								    <div role="tabpanel" class="tab-pane fade" id="trackable">
+									    <div class="css-tab-content">
+										    <div class="row">
+										    	<div class="col-md-6 content-text">
+											    	<h3>Continually disintermediate</h3>
+
+											    	<p>Credibly build virtual materials for fully researched paradigms. Authoritatively plagiarize long-term high-impact infomediaries with goal-oriented core competencies. Conveniently visualize process-centric applications through leading-edge testing procedures. Quickly build backend customer service for real-time metrics.</p> 
+
+													<p>Efficiently transform an expanded array of methodologies rather than customized infomediaries.</p>
+										    	</div><!-- /.col-md-6 -->
+										    	<div class="col-md-6">
+										    		<img class="img-responsive" src="img/track.png" alt="">
+										    	</div><!-- /.col-md-6 -->
+										    </div><!-- /.row -->
+									    </div><!-- /.css-tab-content -->
+								    </div>
+								    <div role="tabpanel" class="tab-pane fade" id="fast">
+									    <div class="css-tab-content">
+										    <div class="row">
+										    	<div class="col-md-6 content-text">
+											    	<h3>Continually disintermediate</h3>
+
+											    	<p>Credibly build virtual materials for fully researched paradigms. Authoritatively plagiarize long-term high-impact infomediaries with goal-oriented core competencies. Conveniently visualize process-centric applications through leading-edge testing procedures. Quickly build backend customer service for real-time metrics.</p> 
+
+													<p>Efficiently transform an expanded array of methodologies rather than customized infomediaries.</p>
+										    	</div><!-- /.col-md-6 -->
+										    	<div class="col-md-6">
+										    		<img class="img-responsive" src="img/fast.png" alt="">
+										    	</div><!-- /.col-md-6 -->
+										    </div><!-- /.row -->
+									    </div><!-- /.css-tab-content -->
+								    </div>
+								    <div role="tabpanel" class="tab-pane fade" id="reliable">
+									    <div class="css-tab-content">
+										    <div class="row">
+										    	<div class="col-md-6 content-text">
+											    	<h3>Continually disintermediate</h3>
+
+											    	<p>Credibly build virtual materials for fully researched paradigms. Authoritatively plagiarize long-term high-impact infomediaries with goal-oriented core competencies. Conveniently visualize process-centric applications through leading-edge testing procedures. Quickly build backend customer service for real-time metrics.</p> 
+
+													<p>Efficiently transform an expanded array of methodologies rather than customized infomediaries.</p>
+										    	</div><!-- /.col-md-6 -->
+										    	<div class="col-md-6">
+										    		<img class="img-responsive" src="img/reliable.png" alt="">
+										    	</div><!-- /.col-md-6 -->
+										    </div><!-- /.row -->
+									    </div><!-- /.css-tab-content -->
+								    </div>
+								  </div>
+								</div><!-- /.css-tab -->
+			              	</div><!-- /.col-md-12 -->
+			              </div><!-- /.row -->
+
+			        	</div><!-- /.container -->
+			        </section>
+			        <!-- why-us-setion end -->
+
+			        <!-- testimonial-section start -->
+					<section class="testimonial-section testimonial2 section-padding">
+			            <div class="container text-center">
+			              <div class="row">
+			                <div class="col-xs-12">
+			                  <h2 class="section-title">We are Trusted By</h2>
+			                  <span class="section-sub">Monotonectally innovate cross-media<br> resources without seamless</span>
+			                </div>
+			              </div> <!-- /.row -->
+
+			              <div class="row row-content">
+			              	<div class="col-md-12">
+								<div id="testimonial-carousel" class="carousel slide" data-ride="carousel">
+
+								  <!-- Indicators -->
+								  <ol class="carousel-indicators">
+								    <li data-target="#testimonial-carousel" data-slide-to="0" class="active"></li>
+								    <li data-target="#testimonial-carousel" data-slide-to="1"></li>
+								    <li data-target="#testimonial-carousel" data-slide-to="2"></li>
+								  </ol>
+
+								  <!-- Wrapper for slides -->
+								  <div class="carousel-inner" role="listbox">
+
+								    <div class="item active">
+								    	
+								    	<div class="testimonial-content">
+								    		<p>Progressively architect prospective imperatives through competitive communities. Professionally productize user<br> friendly networks with out-of-the-box mindshare. Energistically incubate functionalized best practices after 24/365<br> information. Dramatically restore end-to-end strategic theme areas.</p>
+								    		<span class="client-title"></span>
+								    		<span class="client-org"><a href="http://uiCookies" target="_blank">ThemeRox</a></span>
+								    		<span class="client-avatar"><img src="" alt="client avatar"></span>
+								    	</div><!-- /.testimonial-content -->
+								    	
+								    </div>
+								    <div class="item">
+								    	<div class="testimonial-content">
+								    		<p>Progressively architect prospective imperatives through competitive communities. Professionally productize user<br> friendly networks with out-of-the-box mindshare. Energistically incubate functionalized best practices after 24/365<br> information. Dramatically restore end-to-end strategic theme areas.</p>
+								    		<span class="client-title"></span>
+								    		<span class="client-org"><a href="http://uiCookies" target="_blank">ThemeRox</a></span>
+								    		<span class="client-avatar"><img src="" alt="client avatar"></span>
+								    	</div><!-- /.testimonial-content -->
+								    </div>
+								    <div class="item">
+								    	<div class="testimonial-content">
+								    		<p>Progressively architect prospective imperatives through competitive communities. Professionally productize user<br> friendly networks with out-of-the-box mindshare. Energistically incubate functionalized best practices after 24/365<br> information. Dramatically restore end-to-end strategic theme areas.</p>
+								    		<span class="client-title"></span>
+								    		<span class="client-org"><a href="http://uiCookies" target="_blank">ThemeRox</a></span>
+								    		<span class="client-avatar"><img src="" alt="client avatar"></span>
+								    	</div><!-- /.testimonial-content -->
+								    </div>
+								  </div>
+								 
+								</div><!-- /.carousel -->
+			              	</div><!-- /.col-md-12 -->
+			              </div><!-- /.row -->
+
+			              <hr>
+
+			              <div class="partner-section">
+				              <div class="row row-content">
+				              	<div class="col-md-12">
+									<div class="owl-carousel partner-carousel">
+									    <div class="item">
+									    	<a href="#"><img src="img/partner/p1.jpg" alt=""></a>
+									    </div>
+									    <div class="item">
+									    	<a href="#"><img src="img/partner/p2.jpg" alt=""></a>
+									    </div>
+									    <div class="item">
+									    	<a href="#"><img src="img/partner/p3.jpg" alt=""></a>
+									    </div>
+									    <div class="item">
+									    	<a href="#"><img src="img/partner/p4.jpg" alt=""></a>
+									    </div>
+									    <div class="item">
+									    	<a href="#"><img src="img/partner/p5.jpg" alt=""></a>
+									    </div>
+									    <div class="item">
+									    	<a href="#"><img src="img/partner/p1.jpg" alt=""></a>
+									    </div>
+									    <div class="item">
+									    	<a href="#"><img src="img/partner/p2.jpg" alt=""></a>
+									    </div>
+									    <div class="item">
+									    	<a href="#"><img src="img/partner/p3.jpg" alt=""></a>
+									    </div>
+									    <div class="item">
+									    	<a href="#"><img src="img/partner/p4.jpg" alt=""></a>
+									    </div>
+									</div>
+
+
+							        <div class="partner-carousel-navigation">
+							            <a class="prev"><i class="fa fa-angle-left"></i></a>
+							            <a class="next"><i class="fa fa-angle-right"></i></a>
+							        </div><!-- /.partner-carousel-navigation -->
+
+
+				              	</div><!-- /.col-md-12 -->
+				              </div><!-- /.row -->
+			              </div><!-- /.partner-section -->
+			            </div><!-- /.container -->
+			        </section>
+			        <!-- testimonial-section end -->
+
+			        <!-- counter start -->
+			        <section class="counter-section" data-stellar-background-ratio="0.5">
+			        	<div class="container">
+							<div class="row">
+						        <div class="col-sm-4 col-xs-12">
+						          <div class="counter-block">
+						          	<span class="count-description flaticon-boat"><strong class="timer">799</strong>order delivered</span>
+						          </div>
+						        </div> 
+						       <div class="col-sm-4 col-xs-12">
+						          <div class="counter-block">
+						          	<span class="count-description flaticon-international"><strong class="timer">19</strong>order delivered</span>
+						          </div>
+						        </div> 
+						       <div class="col-sm-4 col-xs-12">
+						          <div class="counter-block">
+						          	<span class="count-description flaticon-compass"><strong class="timer">521</strong>order delivered</span>
+						          </div>
+						        </div> 
+					      	</div> <!-- /.row -->
+			        	</div><!-- /.container -->
+			        </section><!-- /.counter-section -->
+			        <!-- counter end -->
+
+			        <!-- cta start -->
+			        
+			        <!-- cta end -->
+
+			        <!-- footer-widget-section start -->
+			        
+			        <!-- footer-widget-section end -->
